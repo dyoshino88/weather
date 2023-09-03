9a0ae2e28ecb11b8f426a444148c2374522da6e4
dyoshino88 <yoshino0707dh@gmail.com>
2023-09-03 17:50:11 +0900

    make it better

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from tortoise import fields
-from tortoise.models import Model
 from pydantic import BaseModel
 
 # モデルを定義
 class CityWeather(BaseModel):
-    id = fields.IntField(pk=True)
+    id: fields.IntField(pk=True)
     city_name = fields.CharField(max_length=255)
     current_weather = fields.JSONField()
     weekly_weather = fields.JSONField()
 
 # ルートモデルを作成
 class City(BaseModel):
+    id: fields.IntField
     city_name: str
