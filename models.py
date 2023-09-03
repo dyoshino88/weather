6a2de210a13f6b1d0d9d4f1ff3d06cf99132d3ea
dyoshino88 <yoshino0707dh@gmail.com>
2023-09-03 18:01:42 +0900

    make it better

--- models.py
+++ models.py
@@ -3,12 +3,12 @@ from pydantic import BaseModel
 
 # モデルを定義
 class CityWeather(BaseModel):
-    id: fields.IntField(pk=True)
+    id: int = fields.IntField(pk=True)
     city_name = fields.CharField(max_length=255)
     current_weather = fields.JSONField()
     weekly_weather = fields.JSONField()
 
 # ルートモデルを作成
 class City(BaseModel):
-    id: fields.IntField
+    id: int = fields.IntField
     city_name: str
