7b82e05c8a68d2e1f7a411f2003e98ac591571ba
dyoshino88 <yoshino0707dh@gmail.com>
2023-09-04 06:10:05 +0900

    make it better

--- models.py
+++ models.py
@@ -12,3 +12,17 @@ class CityWeather(BaseModel):
 class City(BaseModel):
     id: int
     city_name: str
+    
+from tortoise import fields, models
+
+# Tortoise ORM モデルを定義
+class CityWeather(models.Model):
+    id = fields.IntField(pk=True)
+    city_name = fields.CharField(max_length=255)
+    current_weather = fields.JSONField()
+    weekly_weather = fields.JSONField()
+
+# Tortoise ORM モデルを作成
+class City(models.Model):
+    id = fields.IntField(pk=True)
+    city_name = fields.CharField(max_length=255)
