d952503e1f3601e2955d80df7f863e8c38303b00
dyoshino88 <yoshino0707dh@gmail.com>
2023-09-05 09:57:35 +0900

    openweather

--- main.py
+++ main.py
@@ -1,96 +1,32 @@
 from fastapi import FastAPI, HTTPException
-from tortoise.contrib.pydantic import pydantic_model_creator
-from models import CityWeather, City
-from typing import List
 import requests
 import os
-from fastapi.middleware.cors import CORSMiddleware
-from urllib.parse import urlparse
 
 app = FastAPI()
 
-# OpenWeather APIキーを環境変数から取得
+# OpenWeather APIのエンドポイントとAPIキー
+OPENWEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
-if OPENWEATHER_API_KEY is None:
-    raise Exception("OpenWeather APIキーが設定されていません。")
-
-# CORS設定：クロスオリジンリクエストを許可する
-origins = [
-    "http://localhost:3000",
-    "https://weather-app-front-eosin.vercel.app",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# JAWSDB_URLからデータベース接続情報を取得
-db_url = os.getenv("JAWSDB_URL")
-
-if db_url is None:
-    raise Exception("JAWSDB_URLが設定されていません。")
-
-# JAWSDB_URLをパースして必要な情報を取得
-result = urlparse(db_url)
-db_username = result.username
-db_password = result.password
-db_host = result.hostname
-db_port = result.port
-db_name = result.path.lstrip("/")
-
-# PydanticモデルをTortoise ORMモデルから自動生成
-City_Pydantic = pydantic_model_creator(City)
-CityWeather_Pydantic = pydantic_model_creator(CityWeather)
-
-# OpenWeather APIから天気情報を取得する関数
-def get_weather_data(city_name):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={OPENWEATHER_API_KEY}&units=metric"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
-
-# 都市を作成するエンドポイント
-@app.post("/cities/", response_model=CityWeather_Pydantic)
-async def create_city(city: City_Pydantic):
-    # OpenWeather APIから天気情報を取得
-    weather_data = get_weather_data(city.city_name)
-    
-    if weather_data is None:
-        raise HTTPException(status_code=404, detail="City not found")
-
-    # データベースに都市を作成
-    city_obj = await CityWeather.create(
-        city_name=city.city_name,
-        current_weather=weather_data,
-        weekly_weather={},
-    )
-    return city_obj
-
 # 都市の天気情報を取得するエンドポイント
-@app.get("/cities/{city_name}", response_model=CityWeather_Pydantic)
-async def get_city_weather(city_name: str):
-    # データベースから都市の天気情報を取得
-    city_obj = await CityWeather.get_or_none(city_name=city_name)
-    if city_obj is None:
-        raise HTTPException(status_code=404, detail="City not found")
-    return city_obj
+@app.get("/get_weather/{city_name}")
+async def get_weather(city_name: str):
+    params = {
+        "q": city_name,
+        "appid": OPENWEATHER_API_KEY,
+        "units": "metric"  # 温度の単位を摂氏に設定
+    }
 
-# FastAPIアプリケーションを起動
+    response = requests.get(OPENWEATHER_API_URL, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        weather = data.get("weather")[0].get("description")
+        temperature = data.get("main").get("temp")
+        return {"city": city_name, "weather": weather, "temperature": temperature}
+    else:
+        raise HTTPException(status_code=response.status_code, detail="City not found")
+
 if __name__ == "__main__":
-    from tortoise.contrib.fastapi import register_tortoise
-    
-    # Tortoise ORMを初期化し、データベース接続を設定
-    register_tortoise(
-        app,
-        db_url=f"mysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}",
-        modules={"models": ["__main__"]},
-        generate_schemas=True,
-        add_exception_handlers=True,
-    )
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
