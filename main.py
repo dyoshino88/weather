c91a185c81c2054b88e4a099e4c3120d70bc3fc2
dyoshino88 <yoshino0707dh@gmail.com>
2023-09-06 07:11:19 +0900

    DB接続追加

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import requests
 import os
+from tortoise.contrib.pydantic import pydantic_model_creator
+from tortoise.models import Model
+from tortoise import fields
+from tortoise import Tortoise, run_async
+from datetime import date
 
 app = FastAPI()
 
@@ -19,13 +24,42 @@ app.add_middleware(
     allow_headers=["*"],  # 必要なHTTPヘッダーを指定します
 )
 
+# Herokuの環境変数からデータベース接続情報を取得
+db_url = os.getenv("DATABASE_URL")
+
 # OpenWeather APIのエンドポイントとAPIキー
 OPENWEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
-# 都市の天気情報を取得するエンドポイント
-@app.get("/get_weather/{city_name}")
-async def get_weather(city_name: str):
+# Tortoise ORMモデル
+class City(Model):
+    id = fields.IntField(pk=True)
+    city_name = fields.CharField(max_length=255, unique=True)
+
+class CityWeather(Model):
+    id = fields.IntField(pk=True)
+    city_name = fields.CharField(max_length=255, unique=True)
+    weather = fields.CharField(max_length=255)
+    temperature = fields.DecimalField(max_digits=5, decimal_places=2)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    
+Tortoise.init(db_url=db_url)  # Herokuの環境変数からデータベース接続情報を読み込む
+
+Tortoise.init_models(["__main__"], "models")
+City_Pydantic = pydantic_model_creator(City)
+CityWeather_Pydantic = pydantic_model_creator(CityWeather)
+
+# 都市の天気情報を取得してデータベースに格納し、同じ日の同一地域の天気情報をDBから取得するエンドポイント
+@app.get("/get_weather/{city_name}", response_model=CityWeather_Pydantic)
+async def get_and_store_weather(city_name: str):
+    # すでに同じ日の同一地域の情報がDBにあるか確認
+    today = date.today()
+    existing_weather = await CityWeather.filter(city_name=city_name, created_at__date=today).first()
+    
+    if existing_weather:
+        return existing_weather
+    
+    # DBに情報がない場合、OpenWeather APIから取得
     params = {
         "q": city_name,
         "appid": OPENWEATHER_API_KEY,
@@ -38,10 +72,19 @@ async def get_weather(city_name: str):
         data = response.json()
         weather = data.get("weather")[0].get("description")
         temperature = data.get("main").get("temp")
-        return {"city": city_name, "weather": weather, "temperature": temperature}
+
+        # データベースに都市の天気情報を格納
+        city_weather = await CityWeather.create(
+            city_name=city_name,
+            weather=weather,
+            temperature=temperature,
+        )
+
+        return city_weather
     else:
         raise HTTPException(status_code=response.status_code, detail="City not found")
 
 if __name__ == "__main__":
+    Tortoise.generate_schemas()
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))  # Herokuのポートを取得
