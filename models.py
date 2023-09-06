16c8b76bad6bf69934e2a44b5f8c6551a82ec58b
dyoshino88 <yoshino0707dh@gmail.com>
2023-09-06 16:54:22 +0900

    fix

--- models.py
+++ models.py
@@ -1,16 +1,19 @@
 from tortoise.models import Model
-from tortoise import fields
+from tortoise import fields, models
 
 class City(Model):
     id = fields.IntField(pk=True)
     city_name = fields.CharField(max_length=255, unique=True)
 
-class CityWeather(Model):
+class CityWeather(models.Model):
     id = fields.IntField(pk=True)
     city_name = fields.CharField(max_length=255, unique=True)
     weather = fields.CharField(max_length=255)
     temperature = fields.DecimalField(max_digits=5, decimal_places=2)
     created_at = fields.DatetimeField(auto_now_add=True)
-    
+
     class Meta:
-        table = "city_weather_data"  # テーブル名を指定
+        table = "city_weather"
+
+    # モデルにデフォルトの接続を設定
+    default_connection = "default"  # デフォルトの接続名を設定
